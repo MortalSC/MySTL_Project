fcf51c9a220728db52d288e33cba53f5cf258942
bailuo-xiaofeng <Mortal_SCode@163.com>
2023-12-16 17:14:26 +0800

    C++ STL priority_queue 优先级队列实现框架搭建

---
+++ stl_priority_queue/stl_priority_queue/stl_priority_queue/PRIORITY_QUEUE.h
@@ -0,0 +1,8 @@
+#pragma warning (disable : 4996)
+
+namespace STL {
+	template<class T>
+	class priority_queue {
+
+	};
+}
---
+++ stl_priority_queue/stl_priority_queue/stl_priority_queue/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "PRIORITY_QUEUE.h"
+
+int main() {
+	try
+	{
+
+	}
+	catch (const std::exception&)
+	{
+
+	}
+	return 0;
+}
